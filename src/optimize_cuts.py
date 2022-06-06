ea50e8b8e4ecfb7b2c43dfdcba1e6db24c3994fc
Justin Kunimune <justinkunimune@gmail.com>
2022-06-05 22:24:45 -0400

    truly optimal
    I refined it a bit more.  now it doesn't double back on itself, which both saves computation time and means I don't haff to worry about paths that should be identical not being identical.  saving it in a fashion that accounts for this new method also saves on disc space.  I'm very happy with it now.

--- src/optimize_cuts.py
+++ src/optimize_cuts.py
@@ -13,12 +13,12 @@ import os
 import tifffile
 
 
-# what fraction of the found paths should be plotted
-AMOUNT_TO_PLOT = 5e-4
 # how many pixels per degree
-RESOLUTION = 4
+RESOLUTION = 5
 # how to determine the value of a pixel that contains multiple data points
 REDUCTION = np.mean
+# what fraction of the found paths should be plotted
+AMOUNT_TO_PLOT = 2e-2/RESOLUTION**2
 
 
 class Path:
@@ -29,29 +29,10 @@ class Path:
 		self.i = i # the x indices that define this path
 		self.j = j # the y indices that define this path
 		self.z_sorted = sorted(hitemap[i, j]) # the sorted z values that rate this path
+		self.len = len(i)
+		assert self.len >= 1
 		self.start = (i[0], j[0])
 		self.end = (i[-1], j[-1])
-
-
-	def prune(self, hitemap: np.ndarray) -> None:
-		""" edit this path in place to remove any node that is hier than both of its
-		    neibors if its neibors are adjacent to each other.
-		"""
-		for k in range(len(self.i) - 2, 0, -1):
-			di = abs(self.i[k + 1] - self.i[k - 1])
-			dj = abs(self.j[k + 1] - self.j[k - 1])
-			a = hitemap[self.i[k - 1], self.j[k - 1]]
-			b = hitemap[self.i[k], self.j[k]]
-			c = hitemap[self.i[k + 1], self.j[k + 1]]
-			if di <= 1 and dj <= 1 and b > a and b > c:
-				self.i.pop(k)
-				self.j.pop(k)
-				self.z_sorted.remove(b)
-
-
-def bin(x: np.ndarray, bin_edges: np.ndarray) -> np.ndarray:
-	""" I dislike the way numpy defines this function """
-	return np.digitize(x, bin_edges) - 1
 
 
 def bin_centers(bin_edges: np.ndarray) -> np.ndarray:
@@ -59,9 +40,32 @@ def bin_centers(bin_edges: np.ndarray) -> np.ndarray:
 	return (bin_edges[1:] + bin_edges[:-1])/2
 
 
-def round_index(x: float, arr: np.ndarray) -> int:
+def bin_index(x: np.ndarray, bin_edges: np.ndarray) -> np.ndarray:
+	""" I dislike the way numpy defines this function """
+	return np.digitize(x, bin_edges) - 1
+
+
+def round_index(x: float or np.ndarray, arr: np.ndarray) -> int or np.ndarray:
 	""" find the index for arr that yields the value nearest x """
-	return int(np.round(np.interp(x, arr, np.arange(arr.size))))
+	anser = np.round(np.interp(x, arr, np.arange(arr.size)))
+	try:
+		return int(anser)
+	except TypeError:
+		return anser.astype(int)
+
+
+def adjacent(a: tuple[int, int], b: tuple[int, int]) -> bool:
+	""" are these two index pairs adjacent (diagonals count)? """
+	return abs(a[0] - b[0]) <= 1 and abs(a[1] - b[1]) <= 1
+
+
+def index_of(pair: tuple, x: np.ndarray, y: np.ndarray) -> int:
+	""" find the first index of x and y such that pair[0] == x[i] and paint[1] == y[i] """
+	assert len(pair) == 2
+	if np.any((pair[0] == x) & (pair[1] == y)):
+		return np.nonzero((pair[0] == x) & (pair[1] == y))[0][0]
+	else:
+		return -1
 
 
 def load_elevation_data(ф_nodes: np.ndarray, λ_nodes: np.ndarray) -> np.array:
@@ -86,10 +90,10 @@ def load_elevation_data(ф_nodes: np.ndarray, λ_nodes: np.ndarray) -> np.array:
 		# read its location and assine node indices
 		ф0 = float(filename[-6:-4]) * (1 if filename[-7] == "n" else -1)
 		ф_data = bin_centers(np.linspace(ф0, ф0 - 50, z_data.shape[0] + 1))
-		i_data = bin(ф_data, ф_bins)
+		i_data = bin_index(ф_data, ф_bins)
 		λ0 = float(filename[-10:-7]) * (1 if filename[-11] == "e" else -1)
 		λ_data = bin_centers(np.linspace(λ0, λ0 + 40, z_data.shape[1] + 1))
-		j_data = bin(λ_data, λ_bins)%z_nodes.shape[1]
+		j_data = bin_index(λ_data, λ_bins)%z_nodes.shape[1]
 
 		# iterate thru the touchd nodes and assine values
 		for i in np.unique(i_data):
@@ -105,7 +109,7 @@ def load_elevation_data(ф_nodes: np.ndarray, λ_nodes: np.ndarray) -> np.array:
 	return z_nodes
 
 
-def find_hiest_path(start: tuple[float, float], end: tuple[float, float],
+def find_hiest_path(start: tuple[float, float], end: tuple[float, float] or np.ndarray,
                     x_nodes: np.ndarray, y_nodes: np.ndarray, z_nodes: np.ndarray
                     ) -> np.ndarray:
 	""" perform a dijkstra search to find the hiest path between start and end.  a path
@@ -115,17 +119,20 @@ def find_hiest_path(start: tuple[float, float], end: tuple[float, float],
 	    other is hier than the corresponding point on the other.  this means that such
 	    paths will define watershed divides.
 	    :param: start the start point of the search (must be in the node arrays)
-	    :param: end the stop point of the search (must be in the node arrays)
+	    :param: end the stop point or possible stop points of the search (each must be in
+	                the node arrays)
 	    :param: x_nodes a n-long array of allowed x positions
 	    :param: y_nodes a m-long array of allowed y positions
 	    :param: z_nodes a n×m array of the hite value at each pair of x and y
 	    :return: a l×2 array of the x and y coordinate at each point on the hiest path;
 	             the 0th element will be start and the l-1th element will be end.
 	"""
+	if type(end[0]) == float:
+		end = np.atleast_2d(end)
 	i_start = round_index(start[0], x_nodes)
 	j_start = round_index(start[1], y_nodes)
-	i_end = round_index(end[0], x_nodes)
-	j_end = round_index(end[1], y_nodes)
+	i_ends = round_index(end[:, 0], x_nodes)
+	j_ends = round_index(end[:, 1], y_nodes)
 	visited = np.full(z_nodes.shape, False)
 
 	# keep a list of the current paths in progress
@@ -135,8 +142,7 @@ def find_hiest_path(start: tuple[float, float], end: tuple[float, float],
 		# take the most promising one
 		path = candidates.pop()
 		# if it reached the goal, we're all done here
-		if path.end[0] == i_end and path.end[1] == j_end:
-			path.prune(z_nodes)
+		if index_of(path.end, i_ends, j_ends) != -1:
 			plt.close("all")
 			return np.stack([x_nodes[path.i], y_nodes[path.j]], axis=-1)
 		# otherwise, check that no one has beat it here
@@ -149,15 +155,16 @@ def find_hiest_path(start: tuple[float, float], end: tuple[float, float],
 			# and iterate thru all potential follow-ups
 			for di in [-1, 0, 1]:
 				for dj in [-1, 0, 1]:
-					if di != 0 or dj != 0:
+					if di != 0 or dj != 0: # it must move in some direction
 						i_next = i + di
 						j_next = (j + dj + y_nodes.size)%y_nodes.size
 						if i_next > 0 and i_next < x_nodes.size:
-							if not visited[i_next, j_next]:
-								new_path = Path(path.i + [i_next],
-								                path.j + [j_next], z_nodes)
-								bisect.insort(candidates, new_path,
-								              key=lambda path: path.z_sorted)
+							if not visited[i_next, j_next]: # not cross any existing paths
+								if path.len < 2 or not adjacent((i_next, j_next), (path.i[-2], path.j[-2])): # and it must not form an unnecessary detour
+									new_path = Path(path.i + [i_next],
+								                    path.j + [j_next], z_nodes)
+									bisect.insort(candidates, new_path,
+									              key=lambda path: path.z_sorted)
 		if len(paths_to_plot) == 6:
 			plt.clf()
 			for path in paths_to_plot:
@@ -175,7 +182,7 @@ def find_hiest_path(start: tuple[float, float], end: tuple[float, float],
 
 if __name__ == "__main__":
 	# define the start and end locations of the cuts
-	endpoints = [(-36.46, 148.26), (-29.47, 29.27), (-46.60, -73.35)]
+	endpoints = [(-29.47, 29.27), (-46.60, -73.35), (-36.46, 148.26)]
 
 	# define the allowd nodes of the path
 	ф_map = np.linspace(-90, 90, int(180*RESOLUTION + 1))
@@ -184,15 +191,37 @@ if __name__ == "__main__":
 	z_map = load_elevation_data(ф_map, λ_map)
 
 	# search for paths between the endpoints
+	tripoint: tuple[float, float] or None = None
 	paths = []
-	for i in range(len(endpoints)):
-		print(f"finding path from {endpoints[i]} to {endpoints[(i + 1)%len(endpoints)]}")
-		paths.append(find_hiest_path(endpoints[i], endpoints[(i + 1)%len(endpoints)], ф_map, λ_map, z_map))
+	for i in range(1, len(endpoints)):
+		start = endpoints[i]
+		# first, draw a line from endpoint 1 to endpoint 0
+		if len(paths) == 0:
+			end = endpoints[0]
+		# then, draw a line from endpoint 2 to any place on that path
+		elif len(paths) == 1:
+			end = paths[0]
+		# after that, tripoint will be defined and you should always go to that
+		else:
+			end = tripoint
+		print(f"finding path from {start} to {end}")
+		paths.append(find_hiest_path(start, end, ф_map, λ_map, z_map))
+		# remember to define the tripoint after the second path is drawn
+		if len(paths) == 2:
+			tripoint = paths[-1][-1, :]
+
+	# adjust the first path to make it go to the tripoint like the others
+	split_index = index_of(tripoint, paths[0][:, 0], paths[0][:, 1])
+	paths.append(paths[0][split_index:, :])
+	paths[0] = paths[0][:split_index + 1, :]
+	# then flip everything so that they all go away from the tripoint
+	for i in range(len(paths)):
+		paths[i] = paths[i][::-1, :]
 
 	# save and plot them
 	np.savetxt("../spec/continental_divides.txt", np.concatenate(paths), fmt="%.1f")
 	plt.figure()
-	plt.contourf(λ_map, ф_map, z_map, levels=np.linspace(0.5, 10000, 21), vmax=3000)
+	plt.contourf(λ_map, ф_map, z_map, levels=np.linspace(0.5, 10000, 21), vmax=3000, cmap="cividis")
 	plt.contour(λ_map, ф_map, z_map, levels=np.linspace(0.5, 10000, 21), colors="k", linewidths=0.2)
 	for path in paths:
 		plt.plot(path[:, 1], path[:, 0])
