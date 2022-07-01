27fc8ea39c91585b116fe6027a94e96908cdb689
Justin Kunimune <justinkunimune@gmail.com>
2022-07-01 16:56:31 -0400

    land ho
    I did a polygon test so that it can now distinguish between points on land and points at sea.

--- README.md
+++ README.md
@@ -1,5 +1,26 @@
 # elastik
  A new and improved suite of map projections based on a polydimensional mesh
 
+ The Elastic projections are a map projections of a new breed that uses
+ interpolation on a mesh to minimize and control the distortion in maps of the
+ whole Earth like never before.
+
 ## Data sources
- To run the `optimize_cuts.py` script, which locates and saves continental divides for use as interruptions, you will need the USGS EROS Center's global 30 arcsec elevation data set (GTOPO30, DOI: 10.5066/F7DF6PQS).  Download it as GeoTIF files and put it in data/elevation.  The script will load and assemble it automatically.
+ I didn't include the geographic data that some of the code is run on because
+ some of it is kind of big, and it mite need to be updated periodically in the
+ future.
+ 
+ To run the `optimize_cuts.py` script, which locates and saves
+ continental divides for use as interruptions, you will need the USGS EROS
+ Center's global 30 arcsec elevation data set (GTOPO30, DOI: 10.5066/F7DF6PQS).
+ Download it as GeoTIF files and put it in data/elevation/.  The script will load
+ and assemble it automatically.
+ 
+ To run the `calculate_weights.py` script, which computes a map of the distance of
+ each point on earth from the nearest shoreline, you will need the coastline and
+ land polygon datasets from Natural Earth (naturalearthdata.com).   Download the
+ zip files and put them in data/.  The script will unzip them automatically.
+ You specifically need
+ - Natural Earth 10m coastline dataset
+ - Natural Earth 10m minor islands coastline dataset
+ - Natural Earth 110m land dataset
--- src/calculate_weights.py
+++ src/calculate_weights.py
@@ -14,8 +14,8 @@ from matplotlib import pyplot as plt
 from util import bin_centers, to_cartesian
 
 
-COAST_WIDTH = 8 # degrees
-PRECISION = 1 # degrees
+COAST_WIDTH = 10 # degrees
+PRECISION = .5 # degrees
 
 
 def load_coast_vertices(precision) -> list[tuple[float, float]]:
@@ -23,8 +23,8 @@ def load_coast_vertices(precision) -> list[tuple[float, float]]:
 	    too precise about the ceastlines' exact shapes.
 	"""
 	points = []
-	for data in ["coastline"]:#, "minor_islands_coastline"]:
-		λ_last, ф_last = np.nan, np.nan
+	λ_last, ф_last = np.nan, np.nan
+	for data in ["coastline", "minor_islands_coastline"]:
 		with shapefile.Reader(f"../data/ne_10m_{data}.zip") as shapef:
 			for shape in shapef.shapes():
 				for λ, ф in shape.points:
@@ -36,25 +36,60 @@ def load_coast_vertices(precision) -> list[tuple[float, float]]:
 	return points
 
 
+def calculate_coast_distance(ф: np.ndarray, λ: np.ndarray) -> np.ndarray:
+	""" take a set of latitudes and longitudes and calculate the angular distance from
+	    each point to the nearest shoreline (in degrees)
+	"""
+	фф, λλ = np.meshgrid(ф, λ, indexing="ij")
+	xx, yy, zz = to_cartesian(фф, λλ)
+
+	points = load_coast_vertices(PRECISION)
+	minimum_distance = np.full((ф.size, λ.size), np.inf)
+
+	for λ_0, ф_0 in points:
+		x_0, y_0, z_0 = to_cartesian(ф_0, λ_0)
+		minimum_distance = np.minimum(minimum_distance,
+		                              np.degrees(np.arccos(x_0*xx + y_0*yy + z_0*zz)))
+	return minimum_distance
+
+
+def load_land_polygons() -> list[list[tuple[float, float]]]:
+	""" load the land polygon shapefile (110m resolution) """
+	polygons = []
+	with shapefile.Reader(f"../data/ne_110m_land.zip") as shapef:
+		for shape in shapef.shapes():
+			polygons.append(shape.points)
+	return polygons
+
+
+def find_land_mask(ф_grid: np.ndarray, λ_grid: np.ndarray) -> np.ndarray:
+	""" bild a 2D bool array that is True for coordinates on land and False for coordinates in the ocean. """
+	crossings = np.full((ф.size, λ.size), 0)
+	polygons = load_land_polygons()
+	for polygon in polygons:
+		for i in range(len(polygon)):
+			λ_0, ф_0 = polygon[i - 1]
+			λ_1, ф_1 = polygon[i]
+			if λ_0 == λ_1:
+				continue
+			ф_X = ф_0 + (λ_grid - λ_0)/(λ_1 - λ_0)*(ф_1 - ф_0)
+			intersects = np.not_equal(λ_0 < λ_grid, λ_1 < λ_grid)
+			ф_X[~intersects] = np.inf
+			crossings[ф_X[np.newaxis, :] < ф_grid[:, np.newaxis]] += 1
+	return crossings%2 == 1
+
+
 if __name__ == "__main__":
 	ф_edges = bin_centers(np.linspace(-90, 90, 181))
 	λ_edges = bin_centers(np.linspace(-180, 180, 361))
 	ф, λ = bin_centers(ф_edges), bin_centers(λ_edges)
-	фф, λλ = np.meshgrid(ф, λ, indexing="ij")
-	xx, yy, zz = to_cartesian(фф, λλ)
 
-	print("loading...")
-	points = load_coast_vertices(PRECISION)
-	minimum_distance = np.full((ф.size, λ.size), np.inf)
+	coast_distance = calculate_coast_distance(ф, λ)
 
-	print(len(points))
-	for λ0, ф0 in points:
-		x0, y0, z0 = to_cartesian(ф0, λ0)
-		minimum_distance = np.minimum(minimum_distance, np.degrees(np.arccos(x0*xx + y0*yy + z0*zz)))
+	land = find_land_mask(ф, λ)
 
-	importance = np.exp(-minimum_distance/COAST_WIDTH)
+	importance = np.where(land, 1, np.exp(-coast_distance/COAST_WIDTH))
 
+	plt.figure()
 	plt.pcolormesh(λ_edges, ф_edges, importance)
-	points = np.array(points)
-	plt.scatter(points[:, 0], points[:, 1], c="w", s=1)
 	plt.show()
