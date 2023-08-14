b9cdbe3d21a59fcf49c52d760382bfcb059179fe
Justin Kunimune <justinkunimune@gmail.com>
2023-08-13 20:38:09 -0700

    multiple diagrams
    I refactord and expanded the diagrams.  these pictures are still quite incomplete, but there's more than one now.

--- src/draw_diagrams.py
+++ src/draw_diagrams.py
@@ -20,35 +20,61 @@ from create_map_projection import create_map_projection, Mesh, load_coastline_da
 def draw_diagrams():
 	plt.rcParams.update({'font.size': 12})
 
-	elastic_earth = load_mesh("elastic-earth-I")
-	elastic_earth.nodes /= 1e3  # scale down to a realistic map size and change km to cm
-	rectangular = equirectangular_like(elastic_earth)
+	# first compute the Elastic Earth I projection with lower resolution
+	if load_mesh("elastic-earth-I").nodes.shape[1] > 10:
+		build_mesh("oceans", resolution=4)
+		create_map_projection("continents")
+		plt.close("all")
 
-	fig, (ax_left, ax_right) = plt.subplots(1, 2, num="all sections", figsize=(7, 4))
-	draw_section(ax_left, rectangular, 0, nodes=True, edges=True, border=False, graticule=True, coastlines=True)
-	ax_left.set_xlabel("Longitude")
-	ax_left.set_ylabel("Latitude", labelpad=-1)
-	set_ticks(ax_left, spacing=30, fmt="{x:.0f}°")
-	draw_section(ax_right, elastic_earth, 0, nodes=True, edges=True, border=False, graticule=True, coastlines=True)
-	ax_right.set_xlabel("x (at 1:100M scale)")
-	ax_right.set_ylabel("y (at 1:100M scale)", labelpad=11, rotation=-90)
-	set_ticks(ax_right, spacing=5, fmt="{x:.0f} cm", y_ticks_on_right=True)
-	plt.tight_layout()
+	# then draw the diagrams using that new coarse projection
+	mesh = load_mesh("elastic-earth-I")
+	mesh.nodes /= 1e3  # scale down to a realistic map size and change km to cm
+
+	fig, (ax_left, ax_right) = plt.subplots(1, 2, figsize=(7, 4))
+	plot_projection_domains(
+		ax_left, ax_right, mesh, 0,
+		nodes=True, border=False, shading=False, graticule=False, coastlines=False)
 	plt.savefig("../resources/diagram-1.png", dpi=150)
+
+	fig, (ax_left, ax_right) = plt.subplots(1, 2, figsize=(7, 4))
+	plot_projection_domains(
+		ax_left, ax_right, mesh, 0,
+		nodes=True, border=False, shading=True, graticule=True, coastlines=True)
+	plt.savefig("../resources/diagram-2.png", dpi=150)
+
+	fig, (ax_left, ax_right) = plt.subplots(1, 2, figsize=(7, 4))
+	plot_projection_domains(
+		ax_left, ax_right, mesh, 0,
+		nodes=True, border=True, shading=True, graticule=True, coastlines=True)
+	plt.savefig("../resources/diagram-4.png", dpi=150)
 
 	plt.show()
 
 
-def draw_section(ax: Axes, mesh: Mesh, h: int,
-                 nodes: bool, edges: bool, border: bool,
+def plot_projection_domains(ax_left: Axes, ax_right: Axes,
+                            elastic_earth_mesh: Mesh, section_index: int,
+                            nodes: bool, border: bool, shading: bool,
+                            graticule: bool, coastlines: bool) -> None:
+	equirectangular_mesh = equirectangular_like(elastic_earth_mesh)
+	draw_section(ax_left, equirectangular_mesh, section_index, nodes, border, shading, graticule, coastlines)
+	ax_left.set_xlabel("Longitude")
+	ax_left.set_ylabel("Latitude", labelpad=-1)
+	set_ticks(ax_left, spacing=30, fmt="{x:.0f}°")
+	draw_section(ax_right, elastic_earth_mesh, section_index, nodes, border, shading, graticule, coastlines)
+	ax_right.set_xlabel("x (at 1:100M scale)")
+	ax_right.set_ylabel("y (at 1:100M scale)", labelpad=11, rotation=-90)
+	set_ticks(ax_right, spacing=5, fmt="{x:.0f} cm", y_ticks_on_right=True)
+	plt.tight_layout()
+
+
+
+def draw_section(ax: Axes, mesh: Mesh, section_index: int,
+                 nodes: bool, border: bool, shading: bool,
                  graticule: bool, coastlines: bool) -> None:
 	if nodes:
-		ax.scatter(mesh.nodes[h, :, :, 0], mesh.nodes[h, :, :, 1], color="k", s=20, zorder=20)
-	if edges:
-		ax.plot(mesh.nodes[h, :, :, 0], mesh.nodes[h, :, :, 1], color="k", linewidth=1.2, zorder=20)
-		ax.plot(mesh.nodes[h, :, :, 0].T, mesh.nodes[h, :, :, 1].T, color="k", linewidth=1.2, zorder=20)
+		ax.scatter(mesh.nodes[section_index, :, :, 0], mesh.nodes[section_index, :, :, 1], color="k", s=20, zorder=20)
 	if coastlines:
-		project = RegularGridInterpolator([mesh.ф, mesh.λ], mesh.nodes[h, :, :, :],
+		project = RegularGridInterpolator([mesh.ф, mesh.λ], mesh.nodes[section_index, :, :, :],
 		                                  bounds_error=False, fill_value=nan)
 		coastlines = load_coastline_data(reduction=1)
 		for line in coastlines:
@@ -92,10 +118,4 @@ def equirectangular_like(mesh: Mesh) -> Mesh:
 
 
 if __name__ == "__main__":
-	# first compute the Elastic Earth I projection with lower resolution
-	if load_mesh("elastic-earth-I").nodes.shape[1] > 10:
-		build_mesh("oceans", resolution=4)
-		create_map_projection("continents")
-		plt.close("all")
-	# then draw the diagrams using that new coarse projection
 	draw_diagrams()
